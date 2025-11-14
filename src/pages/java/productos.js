2033d61d5251b2c52caee3a09d609d2fc8d050f0
veronica vidal <vidalveropolania@gmail.com>
2025-11-14 13:45:59 -0500

    nuevos cambios backend

---
+++ src/pages/java/productos.js
@@ -0,0 +1,72 @@
+// funcion de cargar productos 
+async function cargarProductos() {
+    try {
+        const response= await fetch("http://localhost:8081/api/productos");
+        const productos= await response.json();
+
+        const grid = document.getElementById("products-grid");
+            grid.innerHTML= productos.map(productos=>`
+                <main>
+                <div class="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300 transform hover:-translate-y-1 product-card"
+                    data-category="laptops"
+                    data-price="${productos.Precio}"
+                    data-product-Id="${productos.productId}">
+
+                    <div class="bg-linear-to-br from-gray-100 to-gray-200 h-48 flex items-center justify-center overflow-hidden">
+                    <img src="${productos.Image}" alt="${productos.Nombre}"
+                    class="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+                    loading="lazy">
+                    <div class="absolute top-3 right-3 bg-red-500 text-white px-2 py-1 rounded-b-full text-xs font-bold">
+                    -15%
+                    </div>
+                    </div>
+
+                    <div class="p-6">
+                    <h3 class="text-lg font-bold  text-gray-900">
+                    ${productos.Nombre}
+                    </h3>
+
+                    <p class="text-sm text-gray-800 mb-4">
+                    ${productos.Descripcion}
+                    </p>
+
+                    <div class="flex items-center justify-between mb-4">
+                    <div>
+                    <span class="text-2xl font-bold text-blue-600">
+                    ${(productos.Precio||0).toLocaleString('es-CO')}
+                    </span>
+                    </div>
+
+                    <div class="flex text-yellow-600">
+                    ⭐️⭐️⭐️⭐️⭐️
+                    </div>
+                    </div>
+
+                    <div class="flex space-x-2">
+                    <button class="ver-detalles=btn bg-green-600 text-white px-3 py-2 rounded-lg hover:bg-green-700 transition duration-300 flex-1 text-sm">
+                    Ver detalles
+                    </button>
+
+                    <button class="add-to-cart-btn  bg-blue-600 text-white px-3 py-2 rounded-lg hover:bg-green-700 transition duration-300 flex-1 text-sm">
+                    Comprar
+                    </button>
+                    </div>
+                    </div>
+                    </div>
+                    </main>
+                    `).join('');
+                    console.log("Productos cargados con exito")
+
+    } catch (error) {
+        console.error("Error al cargar el producto")
+    }
+
+}
+
+cargarProductos();
+
+// cargue automaticamente el producto en html
+
+setInterval(() => {
+    cargarProductos();
+}, 5000); // 5000 ms = 5 segundos
