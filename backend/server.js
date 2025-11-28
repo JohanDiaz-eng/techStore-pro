bd225a633c36390a0c27774f483a1915be8dbcb0
veronica vidal <vidalveropolania@gmail.com>
2025-11-28 17:09:19 -0500

    mas datos de ac

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import ProductosRoute from "./routes/productos.js";
 import userRoutes from './routes/user.js';
 import { loginUsuario } from './controllers/login.js';
 import PerfilRouter from './routes/perfil.js';
+import RecuperarPassword from './routes/recuperar.js'
 const app =express();
 
 // habilitar todas las rutas //
@@ -21,5 +22,6 @@ app.use("/api/productos", ProductosRoute);
 app.use("/api/user", userRoutes);
 app.use("/api/login", loginUsuario);
 app.use("/api/perfil", PerfilRouter);
+app.use("/api/recuperar", RecuperarPassword)
 
 app.listen(8081,()=>console.log('servidor corriendo en http://localhost:8081'));
